068689e633c9805d1957dbcc79075d69cb8ab5e9
moonhr <moonhr153@gmail.com>
2024-06-11 10:55:24 +0900

    객체 동적할당

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const a = () => {
+const a = (name, age, job) => {
   return{
-    name:'',
-    age: 0,
-    job:''
+    name: name,
+    age: age,
+    job: job
   }
 }
 // console.log(a());
 
-const b = a();
+const b = a("메타몽", 22, "홈프로텍터");
+console.log(b);
+b.address = '대전';
 console.log(b);
