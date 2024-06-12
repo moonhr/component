2133eee417c5f5ad7d9c0e07cd0785d43d98be7a
moonhr <moonhr153@gmail.com>
2024-06-12 10:44:57 +0900

    함수가 객체인것을 추론

--- work-4.js
+++ work-4.js
@@ -1,13 +1,22 @@
-const basicData = {
-  name : "",
-  age: 0,
-  job : ""
+//객체를 함수로 바꿈
+const basicData = () => {
+  return {
+    name: "메타몽",
+    age: 13,
+    job: "홈프로텍터"
+  }
 }
 
-class BasicData {
-  constructor(name, age, job) {
-    this.name = name;
-    this.age = age;
-    this.job = job;
+//컴포넌트 기본구조
+const component = () => {
+  //이 함수가 객체라는걸 추론
+  let obj = basicData()
+
+  const decision = () => {
+
   }
+  return
+  `<html>
+
+  </html>`
 }
